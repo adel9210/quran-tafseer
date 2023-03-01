009083e942d201fafd658ace44bbfe3feb64f66b
adelsadek <40858052+adelsadek1@users.noreply.github.com>
2023-03-01 11:36:26 +0200

    - add highlighter

--- src/components/Quran/QuranPage/QuranPage.tsx
+++ src/components/Quran/QuranPage/QuranPage.tsx
@@ -1,27 +1,13 @@
 import './QuranPage.scss';
 import {ForwardedRef, forwardRef} from "react";
-import {Highlighter} from "./Highlighter/Highlighter";
-import {QuranDataPageDetails} from "../../../quranData";
 import {QuranTafseer} from "../QuranTafseer/QuranTafseer";
+import {QuranPageImage} from "./QuranPageImage";
 
 export const QuranPage = forwardRef((props: { pageNumber: string }, ref: ForwardedRef<HTMLDivElement>) => {
     return <div className='quran-page page' ref={ref}>
         <div className='page-content'>
-            {Number(props?.pageNumber) % 2 === 1 && <img className='quran-page__img' src={require(`../../../assets/images/pages/${props.pageNumber}.png`)}
-                 alt='3'/> }
-            {Number(props?.pageNumber) % 2 === 0 && <QuranTafseer/> }
-            {/*{*/}
-            {/*    QuranDataPageDetails.map(({highlighterId, ayaNumber, styles}, index) => {*/}
-            {/*        const [width, top, left] = styles*/}
-            {/*        return <Highlighter*/}
-            {/*            key={ayaNumber + index}*/}
-            {/*            top={top}*/}
-            {/*            left={left}*/}
-            {/*            width={width}*/}
-            {/*            ayaNumber={ayaNumber}*/}
-            {/*            highlighterId={highlighterId}/>*/}
-            {/*    })*/}
-            {/*}*/}
+            {Number(props?.pageNumber) % 2 === 1 && <QuranPageImage pageNumber={props.pageNumber}/>}
+            {Number(props?.pageNumber) % 2 === 0 && <QuranTafseer/>}
         </div>
 
     </div>
---
+++ src/components/Quran/QuranPage/QuranPageImage.tsx
@@ -0,0 +1,25 @@
+import {QuranDataPageDetails} from "../../../quranData";
+import {Highlighter} from "./Highlighter/Highlighter";
+
+interface Props {
+    pageNumber: string
+}
+
+export const QuranPageImage = (props: Props) => {
+    return <>
+        <img className='quran-page__img' src={require(`../../../assets/images/pages/${props.pageNumber}.png`)}
+             alt='3'/>
+        {
+            QuranDataPageDetails.map(({highlighterId, ayaNumber, styles}, index) => {
+                const [width, top, left] = styles
+                return <Highlighter
+                    key={ayaNumber + index}
+                    top={top}
+                    left={left}
+                    width={width}
+                    ayaNumber={ayaNumber}
+                    highlighterId={highlighterId}/>
+            })
+        }
+    </>
+}
